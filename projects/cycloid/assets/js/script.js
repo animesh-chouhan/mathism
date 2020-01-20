5dc3313be755b5841ffb2a2c4fa84a71a960541a
Animesh Chouhan <animeshsingh.iitkgp@gmail.com>
2020-01-20 22:30:24 +0530

    updates

--- projects/cycloid/assets/js/script.js
+++ projects/cycloid/assets/js/script.js
@@ -1,12 +1,17 @@
 var radius = window.innerHeight / 6;
-var circleX = radius;
-var circleY = (3 / 4) * window.innerHeight - radius - 4;
-var theta = 0;
-var vel = 2;
+var circleX = 0;
+var circleY = (3 / 4) * window.innerHeight - radius;
+var theta = -3.1416 / 2;
+
+var vel = 8;
 var omega = vel / radius;
 
+var pointX;
+var pointY;
+let history = [];
+const HIST = 2000 / vel;
+
 function setup() {
-    frameRate(60);
     createCanvas(windowWidth, windowHeight);
 }
 
@@ -21,17 +26,36 @@ function draw() {
     // noFill();
     fill(250);
     stroke('#2E3138');
-    strokeWeight(10);
+    strokeWeight(8);
     circle(circleX, circleY, 2 * radius);
-    circleX += vel;
 
     strokeWeight(20);
-    stroke('#A4C2A5');
-    point(circleX + radius * cos(theta), circleY + radius * sin(theta));
+    stroke('#96B196');
+    pointX = circleX + radius * cos(theta);
+    pointY = circleY + radius * sin(theta);
+    point(pointX, pointY);
+
+
+    var v = createVector(pointX, pointY);
+    history.push(v);
+    // console.log(history)
+    if (history.length > HIST) {
+        history.shift();
+    }
+
+    stroke(164, 194, 165, 180);
+    strokeWeight(10);
+    for (var i = 0; i < history.length; i++) {
+        point(history[i].x, history[i].y);
+    }
+
+    circleX += vel;
     theta += omega;
 
-    if (circleX > windowWidth) {
-        circleX = 0
+    if (circleX > windowWidth + 15) {
+        circleX = 0;
+        theta = -3.1416 / 2;
+        history = [];
     }
 
 }
